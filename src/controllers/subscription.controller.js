eb02ca7026f6cb555c3830e57979cf415802ab5c
ArtiGaund <artigaund2210@gmail.com>
2024-05-08 01:40:06 +0530

    added subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,53 +1,131 @@
 import mongoose, {isValidObjectId} from "mongoose"
-import {Playlist} from "../models/playlist.model.js"
+import {User} from "../models/user.model.js"
+import { Subscription } from "../models/subscription.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
-const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
-
-    //TODO: create playlist
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+   try {
+        if(!channelId){
+            throw new ApiError(400, "Channel id is required")
+        }
+        // check if channel is present or not
+        const channel = await User.findById(channelId)
+        if(!channel){
+            throw new ApiError(400, "Channel does not exist in the database")
+        }
+        // check if user is already subscribed to the channel
+        const alreadySubscribed = await Subscription.findOne({
+            subscriber: req.user?._id,
+            channel: channelId
+        })
+        // if subscribed => unsubscribe
+        if(alreadySubscribed){
+            await Subscription.findByIdAndDelete(
+                alreadySubscribed?._id,
+                {new: true}
+            )
+            return res
+            .status(200)
+            .json(
+                new ApiResponse(200, {}, "Successfully unsubscribed the channel.")
+            )
+        }
+        const userSubscribe = await Subscription.create({
+            subscriber: req.user?._id,
+            channel: channelId
+        })
+        if(!userSubscribe){
+            throw new ApiError(400, "Error while subscibing the channel" )
+        }
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, userSubscribe, "Successfully subscribe to the channel.")
+        )
+   } catch (error) {
+    throw new ApiError(404, error?.message)
+   }
 })
 
-const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
-    //TODO: get user playlists
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+    try {
+        if(!channelId){
+            throw new ApiError(400, "Channel id is required")
+        }
+        // finding channel exist or not
+        const channel = await User.findById(channelId)
+        if(!channel){
+            throw new ApiError(400, "Channel does not exist in the database")
+        }
+        const user = await Subscription.aggregate([
+            {
+                $match: {
+                    channel: channel.user?._id
+                }
+            },
+            {
+                $project:{
+                    channel:1
+                }
+            }
+        ])
+        if(!user){
+            throw new ApiError(400, "Error while fetching subscribers for the channel")
+        }
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, user, "Subscribers list for the channel fetched successfully")
+        )
+    } catch (error) {
+        throw new ApiError(404, error?.message)
+    }
 })
 
-const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    //TODO: get playlist by id
-})
-
-const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-})
-
-const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
-
-})
-
-const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    // TODO: delete playlist
-})
-
-const updatePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    const {name, description} = req.body
-    //TODO: update playlist
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+    try {
+        if(!subscriberId){
+            throw new ApiError(400, "Subscriber id is required")
+        }
+        const subscribedUser = await User.findById(subscriberId)
+        if(!subscribedUser){
+            throw new ApiError(200, "Subscriber does not exist in the database")
+        }
+        const user = await Subscription.aggregate([
+            {
+                $match: {
+                    subscriber: subscribedUser.user?._id
+                }
+            },
+            {
+                $project:{
+                    subscriber: 1,
+                }
+            }
+        ])
+        if(!user){
+            throw new ApiError(400, "Error while fetching subscribers for the channel")
+        }
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, user, "Subscribers list for the channel fetched successfully")
+        )
+    } catch (error) {
+        throw new ApiError(404, error?.message)
+    }
 })
 
 export {
-    createPlaylist,
-    getUserPlaylists,
-    getPlaylistById,
-    addVideoToPlaylist,
-    removeVideoFromPlaylist,
-    deletePlaylist,
-    updatePlaylist
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
 }
